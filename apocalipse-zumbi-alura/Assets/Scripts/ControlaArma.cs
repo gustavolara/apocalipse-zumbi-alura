793c005cc8d3928b7617f5b89baf479b712e9423
Gustavo Lara Vaz <gulara19@gmail.com>
2018-05-24 12:50:40 -0300

    Adicionando arquivos primarios
    > Arquivos iniciais cedidos pelo curso
    > Alterações realizadas durante as aulas

---
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaArma.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaArma : MonoBehaviour {
+
+    public GameObject Bala;
+    public GameObject CanoArma;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Bala, CanoArma.transform.position, CanoArma.transform.rotation);
+        }
+	}
+}
---
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour {
+
+    public GameObject Jogador;
+    public float velocidade = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate(){
+        float distancia = Vector3.Distance( transform.position, Jogador.transform.position );
+        if(distancia > 2.5)
+        {
+            Vector3 direcao = Jogador.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * velocidade * Time.deltaTime);
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
+    }
+}
